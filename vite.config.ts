b362df529b1b2e12a664093d14939eb6648b277c
Hariom <legend27648@Harioms-MacBook-Air.local>
2026-06-24 10:25:35 +0530

    Configure dynamic assets base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/Mystery-Lab/",
+  base: process.env.VERCEL ? "/" : "/Mystery-Lab/",
   plugins: [react()],
   resolve: {
     alias: {
